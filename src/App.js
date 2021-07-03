5850961d81fb8cb5520f98da4916f8ea47809a5a
Hiro <laciferin@gmail.com>
2021-07-04 02:45:51 +0530

    Rest

--- src/App.js
+++ src/App.js
@@ -7,33 +7,17 @@ import Timer from "./components/Timer"
 const Header = () => {
     return (
 
-        <header className="header .d-flex justify-content-around header-light bg-light header-expand-sg text-dark sticky-top">
-
-            <span className="header-logo">
-            <img src={logo} alt="Logo"/>
-            </span>
-            <h4 className="header-Heading strong position-relative d-none d-md-block">Trial Lesson [Grade
-                1-3]</h4>
-            <h2 className="header-Heading strong position-relative d-md-none">Codingal</h2>
-            <Timer/>
+        <header
+            className="header .d-inline-flex  flex-nowrap header-light bg-light text-dark sticky-top">
+            <img className="header-logo order-1" src={logo} alt="Logo"/>
+            <h2 className="header-Heading strong order-2 d-none d-md-inline d-position-relative ">Trial Lesson [Grade
+                1-3] </h2>
+            <h2 className="header-Heading strong order-2 d-md-none d-inline d-position-relative">Codingal</h2>
+            {/*<Timer/>*/}
             <Modal/>
-            <div className="header-endClass">
-                <button type="button" className="btn btn-danger" data-bs-toggle="modal"
-                        data-bs-target="#endClassModal" title="Modal title"
-                        data-content="And here's some amazing content. It's very engaging. Right?">End class
-                </button>
-            </div>
-            {/*<div className="header-components collapse">*/}
-            {/*    <div className="bg-light shadow-3 p-4">*/}
-            {/*    */}
-            {/*</div>*/}
-            {/*</div>*/}
-            {/*position-absolute end-0*/}
-                {/*                    <button className="header-toggler ms-auto bg-dark collapsed" type="button" data-mdb-toggle="collapse"*/}
-            {/*                            data-toggle="collapse"  data-mdb-target=".header-components" aria-controls="header-components"*/}
-            {/*                            aria-expanded="false" aria-label="Toggle navigation" style={{outlineColor:"white"}}>*/}
-            {/*>*/}
-            {/*                    </button>*/}
+            <button type="button" className="btn btn-danger order-3 header-endClass" data-bs-toggle="modal"
+                    data-bs-target="#endClassModal" data-content="">End class
+            </button>
         </header>
     )
 }
@@ -49,5 +33,31 @@ const App = () => {
 }
 
 export default App
+
+
+const Toggler = () => {
+    {/*<div className="header-components collapse">*/
+    }
+    {/*    <div className="bg-light shadow-3 p-4">*/
+    }
+    {/*    */
+    }
+    {/*</div>*/
+    }
+    {/*</div>*/
+    }
+    {/*position-absolute end-0*/
+    }
+    {/*                    <button className="header-toggler ms-auto bg-dark collapsed" type="button" data-mdb-toggle="collapse"*/
+    }
+    {/*                            data-toggle="collapse"  data-mdb-target=".header-components" aria-controls="header-components"*/
+    }
+    {/*                            aria-expanded="false" aria-label="Toggle navigation" style={{outlineColor:"white"}}>*/
+    }
+    {/*>*/
+    }
+    {/*                    </button>*/
+    }
+}
 
 //
